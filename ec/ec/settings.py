d2340119cac69bf8984a507cd7a5a45f9545540a
Atul Tiwari <atultiwarikhj@gmail.com>
2023-08-09 14:33:35 +0100

    fr

--- ec/ec/settings.py
+++ ec/ec/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,6 +121,8 @@ STATIC_URL = '/static/'
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR/"media"
 LOGIN_REDIRECT_URL = "/profile/"
+
+STATIC_ROOT = 'staticfiles'
 
 
 # Default primary key field type
